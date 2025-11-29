7ca23fd986b0cff1350a99cdaf542012c5c47f67
AnandNarra <narra9505@gmail.com>
2025-11-29 15:54:19 +0530

    tykg

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req, res) =>{
-    res.status(200).json({
-        message:"chai aur backend....."
-    })
+     
+    const {fullName , email,username , password} = req.body
+
+    console.log( email);
+    
 })
 
 export {registerUser}
